79fb01b3869f71b9c2bb1fc27deafe35f67a01a6
SuyeonChoi <tndus970120@naver.com>
2025-10-04 21:57:34 +0900

    feat: 기본 재고 감소 로직 작성

---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,28 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockService {
+
+    private final StockRepository stockRepository;
+
+    public StockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity)  {
+        // 1. Stock 조회
+        // 2. 재고를 감소
+        // 3. 갱신된 값을 저장
+
+        Stock stock = stockRepository.findById(id).orElseThrow();
+        stock.decrease(quantity);
+
+        stockRepository.saveAndFlush(stock);
+    }
+}
