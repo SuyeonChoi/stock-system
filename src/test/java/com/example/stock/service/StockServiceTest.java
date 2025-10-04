b276a6fc4645929c1e7f45f131e8c301790fab1a
SuyeonChoi <tndus970120@naver.com>
2025-10-04 22:14:02 +0900

    test: 멀티스레드 테스트 케이스 추가

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -37,6 +41,33 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    public void 동시에_100개의_요청() throws InterruptedException {
+        // 동시에 여러개의 요청을 보내기 위해 멀티 스레드 사용
+        int threadCount = 100;
+        // ExecutorService - 비동기로 실행하는 작업을 단순화하하여 사용할 수 있게 도와주는 Java API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 100개의 요청이 끝날 때까지 기다리기 위해 CountDownLatch 활용
+        // 다른 스레드에서 수행중인 작업이 대기할 수 있도록 도와주는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+        // 100 - (1 * 100) = 0
+        assertEquals(0, stock.getQuantity());
     }
 
 }
